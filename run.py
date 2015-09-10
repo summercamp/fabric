69b47b49c63ea7e3a2d37e1163e7e1b52ba7204e
Szymon Kowalewski <szymon.k@webinterpret.com>
2015-09-10 14:03:30 +0200

    Rolled back the changes.

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from bottle import route, run
 
 @route('/')
 def hello():
-    return "Hello World! I have changed stuff! ;)"
+    return "Hello World!"
 
 run(host='0.0.0.0', port=5000, debug=True)
