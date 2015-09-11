570325fff5af5c39c84a6238c14b08ca742bc44f
Szymon Kowalewski <szymon.k@webinterpret.com>
2015-09-11 08:43:30 +0200

    I have changed stuff.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import cd
+from fabric.api import env
 from fabric.api import execute
 from fabric.api import local
 from fabric.api import run
@@ -22,17 +23,22 @@ REMOTE_CREDS = {
 
 
 @task(default=True)
-def summercamp_update_code_and_restart():
-    execute(_push_changes_to_remote_repo)
-    execute(_update_remote_host, hosts=HOSTS.get('devil'))
+def update_code_and_restart():
+    execute(push_changes_to_remote_repo)
+    env.user = 'summercamp'
+    env.password = ')FINxUyu5ZD#Yo@Xb(mj'
+    execute(update_remote_host, hosts=['s6.mydevil.net'])
 
 
-def _push_changes_to_remote_repo():
+def push_changes_to_remote_repo():
+    print 'pushing changes to repo'
     local("git commit -am'I have changed stuff.'")
-    local('echo git push origin/master')
+    local('git push origin/master')
 
-def _update_remote_host():
-    print 'update'
+
+def update_remote_host():
+    print 'updating remote host'
+    env.warn_only = True
     with cd('fabric'):
         run('pkill -9 -f run.py')
         run('git pull')
